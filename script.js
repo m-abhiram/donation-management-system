c0c7a91dea0726712151da904f7a19c6ae1214c5
Mellacheruvu Abhiram <113190179+m-abhiram@users.noreply.github.com>
2024-04-30 16:03:44 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-import  { getAuth ,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import  {initializeApp}  from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-// import { firebase }from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-
-
-// import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js'
-// import { initializeApp } from "firebase/app";
-// import {firebase} from 'firebase/app';
-// import { getStorage } from "firebase/storage";
-
-
+import  { getAuth ,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import  {initializeApp}  from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { dataBaseOfItems } from "./db.js";
 
 let firebaseConfig = {
   apiKey: "AIzaSyCQqF1yv1Bvodhv2oacOswdtu7aXsV4MJY",
@@ -21,12 +13,8 @@ let firebaseConfig = {
   measurementId: "G-QK39GGVPZM"
 };
 
-const app = initializeApp(firebaseConfig);
-document.addEventListener('DOMContentLoaded', function() {
-  // Firebase initialization code here
-  firebase.initializeApp(firebaseConfig);
 
-});
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
@@ -36,56 +24,6 @@ let container = document.getElementById("profile");
 let main = document.getElementById("item-holder")
 
 const user = auth.currentUser;
-
-
-
-
-    // const storage = app.storage();
-    // const firestore = firebase.firestore();
-    // function uploadImage() {
-    //   const fileInput = document.getElementById('imgInput');
-    //   const file = fileInput.files[0];
-    //   const storageRef = storage.ref(`images/${file.name}`);
-      
-    //   storageRef.put(file).then(() => {
-    //     console.log("Image uploaded successfully!");
-    //   }).catch(error => {
-    //     console.error("Error uploading image: ", error);
-    //   });
-    // }
-
-    // function saveData() {
-    //   const textInput = document.getElementById('textInput');
-    //   const textData = textInput.value;
-
-    //   firestore.collection('data').add({
-    //     textData: textData
-    //   })
-    //   .then(() => {
-    //     console.log("Data saved successfully!");
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error adding document: ", error);
-    //   });
-    // }
-
-    // function retrieveData() {
-    //   firestore.collection('data').get()
-    //   .then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //       console.log(doc.id, " => ", doc.data());
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error getting documents: ", error);
-    //   });
-    // }
-
-
-
-
-
-
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
@@ -101,8 +39,139 @@ onAuthStateChanged(auth, (user) => {
 });
 
 
+// let more_buttons=document.getElementsByClassName("btn_more")
+// for (let i=0;i<more_buttons.length;i++){
+//   more_buttons[i].addEventListener("click",()=>{
+//     onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         // User is signed in, see docs for a list of available properties
+//         alert("You are logged in")
+        
+      
+//       } else {
+//         // User is signed out
+//         alert("You are logged out")
+
+//       }
+//     });
+//   });
+// }
+
+
+
+document.getElementById("newItem").addEventListener("click",()=>{
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+
+      let popUp=document.getElementById("new")
+      popUp.style.display="block"
+    
+    } else {
+      // User is signed out
+      alert("You need to login first")
+      
+    }
+  });
+  
+})
+
+if (localStorage.getItem("dataBaseOfItems")==null){ 
+  localStorage.setItem("dataBaseOfItems","[]")
+}
+let uploadBtn = document.getElementById("upload-item");
+
+uploadBtn.addEventListener("click",()=>{
+  // itemName : document.getElementById("name").value,
+  // phoneNumber : document.getElementById("phoneNumber").value,
+  // location : ,
+  // timeUsed : document.getElementById("timeUsed").value,
+  // condition : document.getElementById("itemCondition").value,
+  // gmail : document.getElementById("userGmail").value
+
+
+  
+  const main=document.getElementById("item-holder")
+  const template = document.getElementById("item-template");
+  const content= template.content.cloneNode(true);
+  main.prepend(content);
+  
+  let inputTag = document.getElementById('imgInput');
+  inputTag.addEventListener("change",function(){
+    const reader = new FileReader();
+    reader.addEventListener("load",()=>{
+      localStorage.setItem("recent-image",reader.result);
+    });
+    reader.readAsDataURL(this.files[0]);
+  })
+
+  document.getElementById("itemNameTemplate").innerHTML = "Name : " + document.getElementById("name").value;
+  document.getElementById("itemLocationTemplate").innerHTML = "Location : "+document.getElementById("item-location").value;
+  document.getElementById("timeUsedTemplate").innerHTML = "Time-used : "+document.getElementById("timeUsed").value;
+  document.getElementById("itemConditionTemplate").innerHTML = "Item Condition : "+document.getElementById("itemCondition").value;
+
+  const recentImageDataUrl =  localStorage.getItem("recent-image");
+  document.getElementById("itemImage").innerHTML='<img id="itemImg" src="" alt="item-image">';
+  document.getElementById("itemImg").setAttribute("src",recentImageDataUrl);
+
+  var new_data = {item : document.getElementById("name").value, location : document.getElementById("item-location").value, time : document.getElementById("timeUsed").value,condition : document.getElementById("itemCondition").value,img : localStorage.getItem("recent-image")};
+
+  var old_data = JSON.parse(localStorage.getItem("dataBaseOfItems"));
+  old_data.push(new_data);
+  localStorage.setItem("dataBaseOfItems",JSON.stringify(old_data));
+  if (localStorage.getItem("dataBaseOfItems")!=null){
+    console.log(localStorage.getItem("dataBaseOfItems"));
+  }
+
+  // div.style.display="none";
+  // document.getElementById("name").value="";
+  // document.getElementById("item-location").value="";
+  // document.getElementById("timeUsed").value="";
+  // document.getElementById("itemCondition").value="";
+
+  // dataBaseOfItems.unshift({item : document.getElementById("name").value, location : document.getElementById("item-location").value, time : document.getElementById("timeUsed").value,condition : document.getElementById("itemCondition").value});
+
+});
+
+
+//to close the upload popup
+document.getElementById("close-upload").addEventListener("click",()=>{
+  let div = document.getElementById("new");
+  div.style.display="none";
+  document.getElementById("name").value="";
+  document.getElementById("item-location").value="";
+  document.getElementById("timeUsed").value="";
+  document.getElementById("itemCondition").value="";
+})
+
+
+//adding the item information to the firebase and then updating the webpage
+function updateData(){
+  let dataBaseOfItems =  JSON.parse(localStorage.getItem("dataBaseOfItems"))
+  for (let i in dataBaseOfItems){
+    console.log("Item : "+dataBaseOfItems[i].item);
+    console.log("Time used : "+dataBaseOfItems[i].time);
+    console.log("Condition : "+dataBaseOfItems[i].condition);
+    console.log("Location : "+dataBaseOfItems[i].location);
+    console.log("Image URL : "+dataBaseOfItems[i].img);
+
+    const main=document.getElementById("item-holder")
+    const template = document.getElementById("item-template");
+    const content= template.content.cloneNode(true);
+    main.prepend(content);
+    document.getElementById("itemNameTemplate").innerHTML = "Item : " + dataBaseOfItems[i].item;
+    document.getElementById("itemLocationTemplate").innerHTML = "Location : "+dataBaseOfItems[i].location;
+    document.getElementById("timeUsedTemplate").innerHTML = "Time-used : "+dataBaseOfItems[i].time;
+    document.getElementById("itemConditionTemplate").innerHTML = "Item Condition : "+dataBaseOfItems[i].condition;
+    const imageUrl = dataBaseOfItems[i].img
+    document.getElementById("itemImage").innerHTML='<img id="itemImg" src="" alt="item-image">';
+    document.getElementById("itemImg").setAttribute("src",imageUrl);
+  }
+}
+window.onload = updateData()
+
+
 let more_buttons=document.getElementsByClassName("btn_more")
-console.log(more_buttons)
 for (let i=0;i<more_buttons.length;i++){
   more_buttons[i].addEventListener("click",()=>{
     onAuthStateChanged(auth, (user) => {
@@ -119,58 +188,3 @@ for (let i=0;i<more_buttons.length;i++){
     });
   });
 }
-
-
-
-document.getElementById("newItem").addEventListener("click",()=>{
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-
-      let popUp=document.getElementById("new")
-      popUp.style.display="block"
-      // flag=true  
-    
-    } else {
-      // User is signed out
-      alert("You need to login first")
-      
-    }
-  });
-  
-})
-
-//to close the upload popup
-document.getElementById("close-upload").addEventListener("click",()=>{
-  let div = document.getElementById("new");
-  div.style.display="none";
-})
-
-
-//adding the item information to the firebase and then updating the webpage
-var files=[];
-var ImgName,ImgUrl;
-document.getElementById("upload-item").onclick = function(e){
-  files = e.target.files;
-  ImgName = document.getElementById("name").ariaValueMax;
-  var uploadTask = firebase.storage().ref("Images/"+ImgName+".png").put(files[0]);
-  uploadTask.on('state_changed',function(snapshot){
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes)*100;
-    document.getElementById
-  }
-  ,
-  function(error){
-    alert("error while saving image");
-  }),
-  function(){
-    uploadTask.snapshot.ref.getDownloadURL().then(function(url){
-      ImgUrl = url
-    });
-    firebase.database().ref('Pictures/'+ImgName).set({
-      Name : ImgName,
-      Link : ImgUrl
-    });
-    alert("image added successfully");
-  }
-}
-
